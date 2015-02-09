8075e3ad1e06a5f0b548e411575c11538895d730
John Deatherage <john@routeoflastresort.com>
2015-02-09 06:44:34 -0800

    initial commit

---
+++ spec/contrail_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Contrail do
+  it 'has a version number' do
+    expect(Contrail::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
